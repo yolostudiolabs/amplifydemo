dc51b773127a793b354902c74aa31e77367000ab
cdosulli <cdosulli@gmail.com>
2020-05-12 23:15:18 -0400

    Added QuestionSet.js

---
+++ src/QuestionSet.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function QuestionSet() {
+  return (
+    <div className="QuestionSet">
+      <p>This is the first question</p>
+      <p>This is the second question</p>
+    </div>
+  );
+}
+
+export default QuestionSet;
